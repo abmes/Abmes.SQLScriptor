971855bb940944bf52ecfb6629a796c468528113
Lyubomir Tonchev <luboice@gmail.com>
2019-12-13 18:14:22 +0200

    log files content type

--- Docker/AwsEcsLauncher.ps1
+++ Docker/AwsEcsLauncher.ps1
@@ -27,7 +27,7 @@ if ($s3LogsBucketName)
 
         Write-Host $key
 
-        Write-S3Object -BucketName $s3LogsBucketName -Key $key -File $logFile.FullName -Region $awsRegion
+        Write-S3Object -BucketName $s3LogsBucketName -Key $key -File $logFile.FullName -ContentType "text/plain" -Region $awsRegion
     }
 
     Write-Host " "
