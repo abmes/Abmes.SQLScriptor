b28701d96b072b4be1687a6804be5235a184e117
Lyubomir Tonchev <luboice@gmail.com>
2019-12-23 21:23:10 +0200

    aws

--- RunAwsEcsTask.ps1
+++ RunAwsEcsTask.ps1
@@ -1,5 +1,8 @@
 Param(
-  [Parameter(Mandatory=$true)]  [ValidateSet("Lambda", "Ecs")] [string] $ServiceType,
+  [Parameter(Mandatory=$false)] [ValidateSet("EC2", "FARGATE")] [string] $LaunchType = "FARGATE",
+  [Parameter(Mandatory=$false)] [string[]] $Subnets,
+  [Parameter(Mandatory=$false)] [string[]] $SecurityGroupIds,
+  [Parameter(Mandatory=$false)] [string[]] $SecurityGroupNames,
   [Parameter(Mandatory=$true)]  [string] $ContainerName,
   [Parameter(Mandatory=$true)]  [string] $ClusterName,
   [Parameter(Mandatory=$true)]  [string] $ConfigLocation,
@@ -31,6 +34,40 @@ function Get-ContainerOverrides()
     return "{`"containerOverrides`":[{`"name`":`"$ContainerName`",`"environment`":[$configLocationEnvVarJson,$scriptLocationEnvVarJson,$databasesEnvVarJson,$versionsOnlyEnvVarJson,$s3LogsBucketNameEnvVarJson]}]}"
 }
 
+function Aws-EC2-GetSecurityGroupIds(
+    [Parameter(Mandatory=$false)] [string[]] $SecurityGroupNames,
+    [Parameter(Mandatory=$false)] [string]   $AwsProfileName,
+    [Parameter(Mandatory=$false)] [string]   $AwsRegion
+)
+{
+    $groupNames = [string]::Join(",", $SecurityGroupNames)
+
+    $result = &aws ec2 describe-security-groups --group-names "$groupNames" --profile "AwsProfileName" --region "$AwsRegion"
+
+    $json = ConvertFrom-Json ([string]::Concat($result))
+
+    return $json.SecurityGroups.GroupId
+}
+
+function Internal-Ecs-GetNetworkConfiguration(
+    [Parameter(Mandatory=$false)] [string[]] $subnets,
+    [Parameter(Mandatory=$false)] [string[]] $securityGroupIds,
+    [Parameter(Mandatory=$false)] [string[]] $securityGroupNames,
+    [Parameter(Mandatory=$false)] [bool] $assignPublicIp
+)
+{
+    if ((!$securityGroupIds) -and ($securityGroupNames))
+    {
+        $securityGroupIds = Aws-EC2-GetSecurityGroupIds -SecurityGroupNames $securityGroupNames
+    }
+
+    $subnetsString = [string]::Join(",", $subnets)
+    $securityGroupIdsString = [string]::Join(",", $securityGroupIds)
+    $assignPublicIpString = if ($assignPublicIp) { "ENABLED" } else { "DISABLED" }
+
+    return "awsvpcConfiguration={subnets=[$subnetsString],securityGroups=[$securityGroupIdsString],assignPublicIp=$assignPublicIpString}"
+}
+
 function Main()
 {
     $containerOverrides = Get-ContainerOverrides
@@ -38,7 +75,16 @@ function Main()
     $tempContainerOverridesFileName = "tempcontaineroverrides.json"
     Set-Content -Path $tempContainerOverridesFileName -Value $containerOverrides
 
-    $runJson = &aws ecs run-task --cluster $ClusterName --task-definition $ContainerName --overrides "file://$tempContainerOverridesFileName" --profile $AwsProfileName --region $AwsRegion
+    if ($LaunchType -eq "EC2")
+    {
+        $networkConfiguration = Internal-Ecs-GetNetworkConfiguration -subnets $Subnets -securityGroupIds $SecurityGroupIds -securityGroupNames $SecurityGroupNames -assignPublicIp $true
+        $runJson = &aws ecs run-task --cluster $ClusterName --launch-type "$LaunchType" --network-configuration "$networkConfiguration" --task-definition $ContainerName --overrides "file://$tempContainerOverridesFileName" --profile $AwsProfileName --region $AwsRegion
+    }
+    else
+    {
+        $runJson = &aws ecs run-task --cluster $ClusterName --launch-type "$LaunchType" --task-definition $ContainerName --overrides "file://$tempContainerOverridesFileName" --profile $AwsProfileName --region $AwsRegion
+    }
+
     $runJson
 
     Remove-Item $tempContainerOverridesFileName
