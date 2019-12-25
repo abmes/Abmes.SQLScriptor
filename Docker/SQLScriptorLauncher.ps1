7cf6773bd42a652e906d5e2ababc683eca36dc7b
Lyubomir Tonchev <luboice@gmail.com>
2019-12-25 15:10:25 +0200

    PowerShell fix

--- Docker/SQLScriptorLauncher.ps1
+++ Docker/SQLScriptorLauncher.ps1
@@ -9,7 +9,6 @@ if ($IsLinux)   { Set-Alias sqlscriptor (Join-Path (Get-Location) sqlscriptor) }
 
 sqlscriptor -logdir $logDir -config "SQLSCRIPTOR_CONFIG_LOCATION" -script "SQLSCRIPTOR_SCRIPT_LOCATION" -databases "SQLSCRIPTOR_DATABASES" -versionsonly "SQLSCRIPTOR_VERSIONSONLY"
 
-
 $s3LogsBucketName = $env:AWS_S3_LOGS_BUCKET_NAME
 
 if ($s3LogsBucketName)
@@ -17,9 +16,9 @@ if ($s3LogsBucketName)
     $awsRegion = $s3LogsBucketName.Split("@")[1]
     $s3LogsBucketName = $s3LogsBucketName.Split("@")[0]
     
-    Write-Host " "
-    Write-Host "Uploading logs to '$s3LogsBucketName' bucket..."
-    Write-Host " "
+    Write-Output " "
+    Write-Output "Uploading logs to '$s3LogsBucketName' bucket..."
+    Write-Output " "
 
     $logFiles = Get-ChildItem -Path $logDir -Recurse -File
     
@@ -27,13 +26,13 @@ if ($s3LogsBucketName)
     {
         $key = $logFile.FullName.Substring($logDir.Length).TrimStart([System.IO.Path]::DirectorySeparatorChar).Replace([System.IO.Path]::DirectorySeparatorChar, "/")
 
-        Write-Host $key
+        Write-Output $key
 
         Write-S3Object -BucketName $s3LogsBucketName -Key $key -File $logFile.FullName -ContentType "text/plain" -Region $awsRegion | Out-Null
         
         Start-Sleep -Seconds 1
     }
 
-    Write-Host " "
-    Write-Host "Done uploading logs."
+    Write-Output " "
+    Write-Output "Done uploading logs."
 }
